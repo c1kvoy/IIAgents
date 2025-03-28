2ff0fd8ce732fa795a058025326fa0de0e48c9f7
Мамашакиров Нуртилек Жанабидинович <nzmamashakirov@edu.hse.ru>
2025-03-29 01:19:46 +0300

    agent is fine-tuned

--- src/llm/agents.py
+++ src/llm/agents.py
@@ -1,19 +1,23 @@
+import logging
 import os
+import re
 from typing import List
 
 import pandas as pd
 from dotenv import load_dotenv
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_experimental.agents import create_pandas_dataframe_agent
 from langchain_openai import ChatOpenAI
 from langgraph.graph import StateGraph, END, START
 from typing_extensions import TypedDict
-import re
 
 from src.llm.utils import clean_code
 
 load_dotenv()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class AgentState(TypedDict):
@@ -22,40 +26,44 @@ class AgentState(TypedDict):
     plots: list
     dataframe: pd.DataFrame
     answer: str
-    plot_data: dict
-    vars: dict
+    code_with_error: str
+    code_error: Exception
+    instructions: str
 
 
 class EDAgent:
-    def __init__(self, llm_: ChatOpenAI):
+    def __init__(self, llm_: ChatOpenAI, msg_memory: int = 3):
         self.llm: ChatOpenAI = llm_
+        self.msg_memory = msg_memory
         graph_builder = StateGraph(AgentState)
-        # graph_builder.add_node("prompt_enhancer", self.__prompt_enhancer)
         graph_builder.add_node("query_analyzer", self.__query_analyzer)
         graph_builder.add_node("pandas_agent", self.__pandas_agent)
         graph_builder.add_node("plot_agent", self.__plot_agent)
         graph_builder.add_node("ml_agent", self.__ml_agent)
         graph_builder.add_node("consultant_agent", self.__consultant_agent)
+        graph_builder.add_node("code_refactoring_agent", self.__code_refactoring_agent)
 
-        # graph_builder.add_edge(START, "prompt_enhancer")
         graph_builder.add_edge(START, "query_analyzer")
         graph_builder.add_conditional_edges("query_analyzer",
                                             self.__query_choose,
                                             {"pandas_agent": "pandas_agent", "ml_agent": "ml_agent",
                                              "plot_agent": "plot_agent", "consultant_agent": "consultant_agent"})
         graph_builder.add_edge("pandas_agent", END)
-        graph_builder.add_edge("ml_agent", END)
-        graph_builder.add_edge("plot_agent", END)
+        graph_builder.add_conditional_edges("ml_agent", self.__error_detector,
+                                            {"code_refactoring_agent": "code_refactoring_agent", "END": END})
+        graph_builder.add_conditional_edges("plot_agent", self.__error_detector,
+                                            {"code_refactoring_agent": "code_refactoring_agent", "END": END})
         graph_builder.add_edge("consultant_agent", END)
+        graph_builder.add_edge("code_refactoring_agent", END)
         self.plots_dir = "../uploads"
         self.dirty_plots_dir = "./dirty_plots"
         self.graph = graph_builder.compile()
 
     def invoke(self, messages: List[BaseMessage], dataframe: pd.DataFrame):
+        logger.info(f"Поступил следующий запрос от пользователя: \n{messages[-1].content}")
         return self.graph.invoke({"dataframe": dataframe, "messages": messages})
 
     def __query_analyzer(self, state: AgentState):
-        print(state["messages"])
         last_message = state["messages"][-1]
         prompt_template = """
       У тебя есть следующий запрос от пользователя:\n
@@ -69,10 +77,12 @@ class EDAgent:
       В ответе должно быть только одно слово из это списка (pd, plot, ml, consult) и больше ничего.
       """
         query_type = self.llm.invoke(prompt_template.format(user_prompt=last_message.content)).content
-        print(query_type)
+        logger.info(f"Тип запроса: {query_type}")
         return {'query_type': query_type}
 
     def __pandas_agent(self, state: AgentState):
+        messages = state["messages"]
+        user_prompt = messages[-1].content
         agent = create_pandas_dataframe_agent(
             self.llm,
             state["dataframe"],
@@ -80,17 +90,19 @@ class EDAgent:
             verbose=True,
             allow_dangerous_code=True
         )
-        prompt_template = """
+        prompt_template = PromptTemplate.from_template("""
       У тебя следующий запрос от пользователя:\n
       {user_prompt}\n
       Дай ответ, предварительно проведя анализ df.
-      В окончательном ответе ничего не упоминай про python-код, свои промежуточные действия. 
-      Дай ответ, который ждет пользователь.
-      """
-        output = agent.invoke(prompt_template.format(user_prompt=state['messages'][-1].content))["output"]
-        print(output)
-        state["messages"].append(AIMessage(output))
-        return {'answer': output}
+      В окончательном ответе ничего не упоминай про python-код, свои промежуточные действия.
+      """)
+        messages[-1] = HumanMessage(prompt_template.format(user_prompt=user_prompt))
+        if self.msg_memory < len(messages):
+            answer = agent.invoke(messages[-self.msg_memory:])["output"]
+        else:
+            answer = agent.invoke(messages)["output"]
+        messages.append(AIMessage(answer))
+        return {'answer': answer, "messages": messages}
 
     def __plot_agent(self, state: AgentState):
         messages = state["messages"]
@@ -109,6 +121,7 @@ class EDAgent:
       """)
         chain = prompt_template_instruction | self.llm
         instructions = chain.invoke({"user_prompt": user_prompt, "df": df, "columns": df.columns}).content
+        logger.info(f"Plot_agent сгенерировал следующие инструкции: \n{instructions}")
         prompt_template_code = PromptTemplate.from_template("""
       Ты мастерски владеешь библиотеками pandas, numpy, matplotlib, seaborn.
       Пользователь отправил следующий запрос:\n
@@ -125,13 +138,14 @@ class EDAgent:
       (то есть каждую переменную, которая важана для анализа ты должен описать словами).
       В ответе пришли только код, который можно сразу запустить.
       """)
-        chain = prompt_template_code | self.llm | clean_code
+        chain = prompt_template_code | self.llm | StrOutputParser() | (
+            lambda response: response.replace("```python", "").replace("```", ""))
         code = chain.invoke({"user_prompt": user_prompt,
                              "df": df,
                              "columns": df.columns,
                              "instructions": instructions,
                              "dirty_plots_dir": self.dirty_plots_dir})
-        print(code)
+        logger.info(f"Plot_agent сгенерировал следующий код: \n{code}")
         data = {}
         plots = []
         vars = {"df": df, "plots": plots, "data": data}
@@ -140,9 +154,12 @@ class EDAgent:
             data = vars["data"]
             plots = vars["plots"]
         except Exception as e:
-            print(e)
+            logger.info(f"Словили ошибку: {e}")
             return {
-                "answer": "Извините, во время анализа произошла техническая ошибка, попробуйте ввести запрос снова."}
+                "code_error": e,
+                "code_with_error": code,
+                "instructions": instructions
+            }
 
         prompt_template_answer = PromptTemplate.from_template("""
       Пользователь отправил следующий запрос:\n
@@ -154,8 +171,8 @@ class EDAgent:
       Исходя из этих значений, ответь на вопрос пользователя (твой ответ пойдет непосредственно пользователю).
       В ответе не отсылайся к python-коду. Сделай только вывод.
       """)
-        chain = prompt_template_answer | self.llm
-        answer = chain.invoke({"user_prompt": user_prompt, "df": df, "vars": vars, "data": data}).content
+        chain = prompt_template_answer | self.llm | StrOutputParser()
+        answer = chain.invoke({"user_prompt": user_prompt, "df": df, "vars": vars, "data": data})
         plots_paths = self.__save_plots(plots)
         return {"answer": answer, "plots": plots_paths}
 
@@ -175,8 +192,9 @@ class EDAgent:
           Не пиши код, распиши только шаги решения. Твой ответ я передам программисту, который
           с помощью твоих инструкций реализует это в коде.
           """)
-        chain = prompt_template_instruction | self.llm
-        instructions = chain.invoke({"user_prompt": user_prompt, "df": df, "columns": df.columns}).content
+        chain = prompt_template_instruction | self.llm | StrOutputParser()
+        instructions = chain.invoke({"user_prompt": user_prompt, "df": df, "columns": df.columns})
+        logger.info(f"ML_agent сгенерировал следующие инструкции: \n{instructions}")
         prompt_template_code = PromptTemplate.from_template("""
           Ты мастерски владеешь библиотеками sklearn, pandas, numpy, matplotlib, seaborn.
           Пользователь отправил следующий запрос:\n
@@ -193,13 +211,14 @@ class EDAgent:
           (то есть каждую переменную, которая важана для анализа ты должен описать словами).
           В ответе пришли только код, который можно сразу запустить.
           """)
-        chain = prompt_template_code | self.llm | clean_code
+        chain = prompt_template_code | self.llm | StrOutputParser() | (
+            lambda response: response.replace("```python", "").replace("```", ""))
         code = chain.invoke({"user_prompt": user_prompt,
                              "df": df,
                              "columns": df.columns,
                              "instructions": instructions,
                              "dirty_plots_dir": self.dirty_plots_dir})
-        print(code)
+        logger.info(f"ML_agent сгенерировал следующий код: \n{code}")
         data = {}
         plots = []
         vars = {"df": df, "plots": plots, "data": data}
@@ -208,9 +227,12 @@ class EDAgent:
             data = vars["data"]
             plots = vars["plots"]
         except Exception as e:
-            print(e)
+            logger.info(f"Словили ошибку: {e}")
             return {
-                "answer": "Извините, во время анализа произошла техническая ошибка, попробуйте ввести запрос снова."}
+                "code_error": e,
+                "code_with_error": code,
+                "instructions": instructions
+            }
 
         prompt_template_answer = PromptTemplate.from_template("""
       Пользователь отправил следующий запрос:\n
@@ -222,8 +244,8 @@ class EDAgent:
       Исходя из этих значений, ответь на вопрос пользователя (твой ответ пойдет непосредственно пользователю).
       В ответе не отсылайся к python-коду. Сделай только вывод.
       """)
-        chain = prompt_template_answer | self.llm
-        answer = chain.invoke({"user_prompt": user_prompt, "df": df, "vars": vars, "data": data}).content
+        chain = prompt_template_answer | self.llm | StrOutputParser()
+        answer = chain.invoke({"user_prompt": user_prompt, "df": df, "vars": vars, "data": data})
         plots_paths = self.__save_plots(plots)
         return {"answer": answer, "plots": plots_paths}
 
@@ -242,6 +264,56 @@ class EDAgent:
         response = self.llm.invoke(messages).content
         return {"answer": response}
 
+    def __code_refactoring_agent(self, state: AgentState):
+        messages = state["messages"]
+        user_prompt = messages[-1].content
+        code_with_error = state["code_with_error"]
+        code_error = state["code_error"]
+        instructions = state["instructions"]
+        prompt_template = PromptTemplate.from_template(f"""
+        Пользователь отправил следующий запрос:\n
+        {user_prompt}\n
+        Мне дали такие инструкции:\n
+        {instructions}\n
+        Я написал такой python-код:\n
+        {code_with_error}\n
+        К сожалению, я словил следующую ошибку:\n
+        {code_error}\n
+        Из-за этого мне очень грустно и тяжело. Пожалуйста, подними мне настроение и напиши в ответе исправный код.
+        Твой ответ должен содержать только исправный код, который я могу сразу  же запустить без каких-либо изменений.
+        """)
+        chain = prompt_template | self.llm | StrOutputParser() | (
+            lambda response: response.replace("```python", "").replace("```", ""))
+        code = chain.invoke(
+            {"instructions": instructions, "code_with_error": code_with_error, "code_error": code_error})
+        logger.info(f"Code_refactoring_agent сгенерировал следующий код: \n{code}")
+        df = state["dataframe"]
+        data = {}
+        plots = []
+        vars = {"df": df, "plots": plots, "data": data}
+        try:
+            exec(code, vars)
+            data = vars["data"]
+            plots = vars["plots"]
+        except Exception as e:
+            logger.info(f"Словили ошибку в рефакторе: {e}")
+            return {
+                "answer": "Извините, во время анализа произошла техническая ошибка, попробуйте ввести запрос снова."}
+        prompt_template_answer = PromptTemplate.from_template("""
+              Пользователь отправил следующий запрос:\n
+              {user_prompt}\n
+              У тебя есть датафрейм: {df}\n
+              Мы уже провели анализ и получили следующие результаты:\n
+              {vars} - перемнные, которые мы получили из кода-анализатора\n
+              {data} - описание переменных\n
+              Исходя из этих значений, ответь на вопрос пользователя (твой ответ пойдет непосредственно пользователю).
+              В ответе не отсылайся к python-коду. Сделай только вывод.
+              """)
+        chain = prompt_template_answer | self.llm | StrOutputParser()
+        answer = chain.invoke({"user_prompt": user_prompt, "df": df, "vars": vars, "data": data})
+        plots_paths = self.__save_plots(plots)
+        return {"answer": answer, "plots": plots_paths}
+
     def __query_choose(self, state: AgentState):
         query_type = state["query_type"]
         if query_type == "pd":
@@ -251,6 +323,11 @@ class EDAgent:
         if query_type == "ml":
             return "ml_agent"
         return "consultant_agent"
+
+    def __error_detector(self, state: AgentState):
+        if "code_error" in state:
+            return "code_refactoring_agent"
+        return "END"
 
     def __save_plots(self, plots: list) -> list:
         plots_names = []
